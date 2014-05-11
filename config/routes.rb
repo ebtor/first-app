b813d8e0f8e8461d3059e64f23f48904e2d24eb7
Ebony Marshall <evmjvg@gmail.com>
2014-05-11 14:59:18 +0000

    Updated config.ru to say#hello *2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 FirstAp::Application.routes.draw do
   get "say/hello"
+  root to: 'say#hello'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
